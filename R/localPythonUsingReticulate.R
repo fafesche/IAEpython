f7c91650637c1344f1437cb2193bf9e87f5f1f2a
Fabien FESCHET <fabien.feschet@u-auvergne.fr>
2024-10-30 10:46:28 +0100

    TMP: check launching spyder under linux -> currently failed but work in console.

--- R/localPythonUsingReticulate.R
+++ R/localPythonUsingReticulate.R
@@ -143,8 +143,8 @@ IAE.fn.python <- function(course = NULL) {
         system2(path.expand(paste0(IAE.env, "/", myEnv.name, "/Scripts/spyder.exe")), wait = FALSE)
       } else {
         cmd <- paste0("/usr/bin/bash --rcfile <(echo '", 
-               "source ", path.expand(paste0(IAE.env, "/", myEnv.name, "/activate")), 
-               "; spyder'")
+               "source ", path.expand(paste0(IAE.env, "/", myEnv.name, "/bin/activate")), 
+               "; spyder)'")
         print(cmd)
         system2(cmd, wait = FALSE)
       }
